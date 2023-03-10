14807367f469aa865b851a14c12eb88f3135395d
DemoJameson <demojameson@gmail.com>
2023-03-10 17:17:27 +0800

    feat: info hud add tas input

--- SmolAme.YetAnotherTAS/Components/InfoHud.cs
+++ SmolAme.YetAnotherTAS/Components/InfoHud.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 using BepInEx.Configuration;
+using TAS;
+using TAS.Core.Input;
 using UnityEngine;
 
 namespace SmolAme.YetAnotherTAS.Components;
@@ -13,13 +17,13 @@ public class InfoHud : PluginComponent {
     private static ConfigEntry<bool> optionEnabled;
     private static ConfigEntry<Alignment> optionAlignment;
     private static ConfigEntry<int> optionFontSize;
-    private static float Margin => optionFontSize.Value;
-    private static int Padding => optionFontSize.Value;
+    private static float Margin => optionFontSize.Value * 0.7f;
+    private static int Padding => (int) Math.Round(optionFontSize.Value * 0.7f, MidpointRounding.AwayFromZero);
 
     private void Awake() {
-        optionEnabled = Plugin.Instance.Config.Bind("Info HUD", "Info HUD", true);
-        optionAlignment = Plugin.Instance.Config.Bind("Info HUD", "Info HUD Alignment", Alignment.TopRight);
-        optionFontSize = Plugin.Instance.Config.Bind("Info HUD", "Info HUD Font Size", 16, new ConfigDescription("", new AcceptableValueRange<int>(8, 40)));
+        optionEnabled = Plugin.Instance.Config.Bind("Info", "Info HUD", true);
+        optionAlignment = Plugin.Instance.Config.Bind("Info", "Info HUD Alignment", Alignment.TopLeft);
+        optionFontSize = Plugin.Instance.Config.Bind("Info", "Info HUD Font Size", 16, new ConfigDescription("", new AcceptableValueRange<int>(8, 40)));
     }
 
     private void OnGUI() {
@@ -27,9 +31,12 @@ public class InfoHud : PluginComponent {
             return;
         }
 
-        GUIContent content = new(GameInfoHelper.Info);
+        string tasInput = GetTasInput();
+        GUIContent content = new(tasInput + GameInfoHelper.Info);
 
-        style ??= new GUIStyle(GUI.skin.box);
+        style ??= new GUIStyle(GUI.skin.box) {
+            font = Font.CreateDynamicFontFromOSFont("Consolas", 16)
+        };
         style.alignment = TextAnchor.MiddleLeft;
         style.padding = new RectOffset(Padding, Padding, Padding, Padding);
         style.fontSize = optionFontSize.Value;
@@ -60,5 +67,50 @@ public class InfoHud : PluginComponent {
         }
         
         GUI.Box(new Rect(x, y, size.x, size.y), content.text, style);
+    }
+    
+    private static string GetTasInput() {
+        StringBuilder result = new();
+        InputController controller = Manager.Controller;
+        List<InputFrame> inputs = controller.Inputs;
+        if (Manager.Running && controller.CurrentFrameInTas >= 0 && controller.CurrentFrameInTas < inputs.Count) {
+            InputFrame current = controller.Current;
+            if (controller.CurrentFrameInTas >= 1 && current != controller.Previous) {
+                current = controller.Previous;
+            }
+
+            InputFrame previous = current.Previous;
+            InputFrame next = current.Next;
+
+            int maxLine = Math.Max(current.Line, Math.Max(previous?.Line ?? 0, next?.Line ?? 0)) + 1;
+            int linePadLeft = maxLine.ToString().Length;
+
+            int maxFrames = Math.Max(current.Frames, Math.Max(previous?.Frames ?? 0, next?.Frames ?? 0));
+            int framesPadLeft = maxFrames.ToString().Length;
+
+            string FormatInputFrame(InputFrame inputFrame) {
+                return
+                    $"{(inputFrame.Line + 1).ToString().PadLeft(linePadLeft)}: {string.Empty.PadLeft(framesPadLeft - inputFrame.Frames.ToString().Length)}{inputFrame}";
+            }
+
+            if (previous != null) {
+                result.AppendLine(FormatInputFrame(previous));
+            }
+
+            string currentStr = FormatInputFrame(current);
+            int currentFrameLength = controller.CurrentFrameInInput.ToString().Length;
+            int inputWidth = currentStr.Length + currentFrameLength + 2;
+            inputWidth = Math.Max(inputWidth, 20);
+            result.AppendLine(
+                $"{currentStr.PadRight(inputWidth - currentFrameLength)}{controller.CurrentFrameInInput}{current.RepeatString}");
+
+            if (next != null) {
+                result.AppendLine(FormatInputFrame(next));
+            }
+
+            result.AppendLine();
+        }
+
+        return result.ToString();
     }
 }
