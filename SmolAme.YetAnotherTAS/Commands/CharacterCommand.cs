2d26b2a912c1be6d180bae25253cdb071c397005
Shir0Yaksha <85480331+Sh1r0Yaksha@users.noreply.github.com>
2023-05-27 16:39:47 +0530

    Fixed Character selection for versions above 210218 (new physics)

--- SmolAme.YetAnotherTAS/Commands/CharacterCommand.cs
+++ SmolAme.YetAnotherTAS/Commands/CharacterCommand.cs
@@ -27,23 +27,18 @@ public class CharacterCommand {
     [TasCommand("Character", LegalInMainGame = false)]
     private static void Character(string[] args) {
 
+        //if (VersionText.Version() <= 210218) {
+        //    return;
+        //    }
         if (args.IsEmpty()) {
             return;
         }
-
-        SetNumCharacters(args[0], PlayerScript.player.characterPacks.Count);
-    }
-
-    private static void SetCharacter(int index) {
-        MainScript.currentCharacter = index;
-        PlayerPrefs.SetInt("character", MainScript.currentCharacter);
-        PlayerScript.player.currentCharacter = MainScript.currentCharacter;
-    }
-
-    private static void SetNumCharacters(string args ,int numCharacters) {
-        if (int.TryParse(args, out int index)) {
-            index = Mathf.Clamp(index, 1, numCharacters) - 1;
-            SetCharacter(index);
+        if (int.TryParse(args[0], out int index) && PlayerScript.player.characterPacks != null) {
+            index = Mathf.Clamp(index, 1, PlayerScript.player.characterPacks.Count) - 1;
+            MainScript.currentCharacter = index;
+            PlayerPrefs.SetInt("character", MainScript.currentCharacter);
+            PlayerScript.player.currentCharacter = MainScript.currentCharacter;
         }
+
     }
 }
